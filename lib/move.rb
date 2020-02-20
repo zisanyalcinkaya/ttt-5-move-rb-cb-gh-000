eb50307af994982ac597a7ece896e75cb10b1b26
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 09:17:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 # code your input_to_index and move method here!
 
 def update_array_at_with(array, index, value)
-  array[index.to_i-1] = value
+  array[(index.to_i-1)] = value
 end
 
 #update_array_at_with(board, 0, "X")
